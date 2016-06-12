054082a5b64ed5d6a6385c705341872dea8d98e4
yadro <yadrone@gmail.com>
2016-06-12 17:20:19 +0300

    work!

--- projects/simplex/build/main.js
+++ projects/simplex/build/main.js
@@ -5,7 +5,7 @@ require.config({
         'react-dom': '../node_modules/react-dom/dist/react-dom',
         fraction: '../node_modules/fraction.js/fraction',
         'snapsvg' : '../node_modules/snapsvg/dist/snap.svg',
-        'filesaver': '../node_modules/filesaver.js/FileSaver'
+        filesaver: '../node_modules/filesaver.js/filesaver'
     }
 });
 
